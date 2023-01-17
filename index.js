b1a99b845a6f37ece481cf5e136c0fbb14e99099
Shivam Tiwari <93584553+amshiv057@users.noreply.github.com>
2023-01-17 18:49:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
+"P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i",
+"j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", 
+"3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_",
+"-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+let res =""
+let getEl=document.getElementById("get-E");
+function generate(){
+    for(let i=0;i<15;i++){
+        res+=characters[Math.floor(Math.random()*characters.length)];
+    }
+     getEl.textContent=res
+     res=""
+}
